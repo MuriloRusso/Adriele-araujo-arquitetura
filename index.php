a68c6629b0c63352a48abda190fa0504ec0d2291
Murilo Russo <murilorussooo@gmail.com>
2023-10-10 11:01:58 -0300

    adicionado section de sobre

--- index.php
+++ index.php
@@ -17,43 +17,70 @@
 
     <section id="banner" class="carousel slide carousel-fade carousel-banner" data-ride="carousel">
 
-    <!-- <ol class="carousel-indicators">
-        <li data-target=".carousel-banner" data-slide-to="0" class="active"></li>
-        <li data-target=".carousel-banner" data-slide-to="1" class=""></li>
-        <li data-target=".carousel-banner" data-slide-to="2" class=""></li>
+        <!-- <ol class="carousel-indicators">
+            <li data-target=".carousel-banner" data-slide-to="0" class="active"></li>
+            <li data-target=".carousel-banner" data-slide-to="1" class=""></li>
+            <li data-target=".carousel-banner" data-slide-to="2" class=""></li>
 
-    </ol> -->
+        </ol> -->
 
-    <div class="carousel-inner">
+        <div class="carousel-inner">
 
-        <div class="carousel-item bg-a-ferramenta-ideal active" data-step="1">
-            <!-- <div class="mt-n5 container h-100 d-flex flex-column align-items-center align-items-lg-end justify-content-center justify-content-lg-right"> -->
-                <img src="https://static.wixstatic.com/media/4bd675_3a0e3b90234f47e59a2ece1f43562820~mv2.jpg/v1/fill/w_1817,h_675,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4bd675_3a0e3b90234f47e59a2ece1f43562820~mv2.jpg" alt="Logo Ruckus Commscope e Elite" title="Ruckus an ARRIS company" class="img-fluid">
+            <div class="carousel-item bg-a-ferramenta-ideal active" data-step="1">
+                    <img src="https://static.wixstatic.com/media/4bd675_3a0e3b90234f47e59a2ece1f43562820~mv2.jpg/v1/fill/w_1817,h_675,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4bd675_3a0e3b90234f47e59a2ece1f43562820~mv2.jpg" alt="Logo Ruckus Commscope e Elite" title="Ruckus an ARRIS company" class="img-fluid">
 
-            <!-- </div> -->
+            </div>
+            <div class="carousel-item bg-extreme" data-step="2">
+                    <img src="https://static.wixstatic.com/media/4bd675_5aaa9c6ca9044b99852b8d1ebb4f4c44~mv2.jpg/v1/fill/w_1817,h_675,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4bd675_5aaa9c6ca9044b99852b8d1ebb4f4c44~mv2.jpg" alt="Logo Ruckus Commscope e Elite" title="Ruckus an ARRIS company" class="img-fluid">
+
+            </div>
+            <div class="carousel-item bg-ruckus" data-step="3">
+                    <img src="https://static.wixstatic.com/media/4bd675_729e798c1cf94e1db4be74ba7c890d69~mv2.jpg/v1/fill/w_1817,h_675,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4bd675_729e798c1cf94e1db4be74ba7c890d69~mv2.jpg" alt="Logo Ruckus Commscope e Elite" title="Ruckus an ARRIS company" class="img-fluid">
+            </div>
+
+            <!-- <a class="carousel-control-prev" href="#banner" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#banner" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a> -->
+
         </div>
-        <div class="carousel-item bg-extreme" data-step="2">
-            <!-- <div class="mt-n5 container h-100 d-flex flex-column align-items-center align-items-lg-start justify-content-center"> -->
-                <img src="https://static.wixstatic.com/media/4bd675_5aaa9c6ca9044b99852b8d1ebb4f4c44~mv2.jpg/v1/fill/w_1817,h_675,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4bd675_5aaa9c6ca9044b99852b8d1ebb4f4c44~mv2.jpg" alt="Logo Ruckus Commscope e Elite" title="Ruckus an ARRIS company" class="img-fluid">
 
-            <!-- </div> -->
+    </section>
+
+    <section class='pt container'>
+
+        <div class="container text-center">
+
+            <h2>Sobre</h2>
+            <hr>
+
         </div>
-        <div class="carousel-item bg-ruckus" data-step="3">
-            <!-- <div class="mt-n5 container h-100 d-flex flex-column align-items-l justify-content-center"> -->
-                <img src="https://static.wixstatic.com/media/4bd675_729e798c1cf94e1db4be74ba7c890d69~mv2.jpg/v1/fill/w_1817,h_675,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/4bd675_729e798c1cf94e1db4be74ba7c890d69~mv2.jpg" alt="Logo Ruckus Commscope e Elite" title="Ruckus an ARRIS company" class="img-fluid">
-            <!-- </div> -->
+
+        <div class="container-2">
+
+            <div class="row">
+
+                <div class="col-md-6 col-12">
+
+                    <img src="https://static.wixstatic.com/media/4bd675_60ef2e89aba147b18113d63aa9bb4090~mv2.jpg/v1/crop/x_0,y_0,w_3469,h_5063/fill/w_347,h_506,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/VF1_0162_JPG.jpg" alt="">
+
+                </div>
+
+                <div class="col-md-6 col-12">
+
+                    <p>A LOFT 7 ARQUITETURA é um escritório voltado para Design de Interiores que tem como premissa essencial a busca por projetos que proponham integração entre o ambiente construído e as características de cada Cliente.</p>
+
+                    <p>A busca pelo bem estar, criando ambientes de encontro e a interpretação do contexto com o uso de materiais naturais, que são elementos explorados por nossos projetos! Rafaella Grasnoff é a Arquiteta a frente do escritório, com um estilo jovial e moderno está sempre em busca de inovações e idealiza sensações agradáveis em cada ambiente desenvolvido!</p>
+
+                </div>
+
+            </div>
+
         </div>
-
-        <!-- <a class="carousel-control-prev" href="#banner" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#banner" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a> -->
-
-    </div>
 
     </section>
 
