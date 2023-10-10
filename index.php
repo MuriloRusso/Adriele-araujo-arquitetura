9b4220447f69ed6375dbe9f8c5055ed0bffa1cf4
Murilo Russo <murilorussooo@gmail.com>
2023-10-10 09:19:49 -0300

    Projeto Iniciado

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Adriele Araújo Arquitetura</title>
+
+    <?php include('public/includes/head.php'); ?>
+</head>
+<body>
+    
+</body>
+</html>
